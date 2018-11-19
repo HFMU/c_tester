e1034c2ae9d5168113209de459c8d9dd47b80333
Casper Thule <casper.thule@eng.au.dk>
2018-11-19 21:16:46 +0100

    Updated

--- hfmu_c_tester.c
+++ hfmu_c_tester.c
@@ -1,26 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
-typedef int (*cfoo)(int);
+#include "fmi/fmi2functions.h"
+
+typedef int (*foo)(int);
+typedef bool (*instantiateLEG)(char[]);
+typedef void* (*instantiate)(char[]);
+typedef int (*printEnv)(void*);
+typedef bool (*updateEnv)(void*);
+
+void cbLogger(fmi2ComponentEnvironment cEnv, fmi2String instanceName, fmi2Status status, fmi2String category, fmi2String message, ...){
+  printf("msg: %s", message);
+}
+
+
+
 void test(void) {
+  // Load the library
   void *handle;
-  cfoo cfooF;
-
-  handle = dlopen("/Users/casperthule/source/haskell/HFMU/dist/build/HFMU/libHFMU.dylib", RTLD_LAZY);
-
+  handle = dlopen("/Users/casperthule/source/haskell/HFMU/HFMU/dist/build/HFMU/libHFMU.dylib", RTLD_LAZY);
   if(!handle){
       fprintf(stderr, "%s\n", dlerror());
       exit(EXIT_FAILURE);
   }
-
   dlerror();
-    cfooF = dlsym(handle, "cfoo");
+  // Setup experiment
+  fmi2Type type = fmi2CoSimulation;
+  fmi2CallbackFunctions cbFuncs = {.logger = &cbLogger, .allocateMemory = NULL, .freeMemory = NULL, .stepFinished = NULL, .componentEnvironment = NULL};
+  fmi2InstantiateTYPE* fmi2Instantiate;
+  fmi2Instantiate = dlsym(handle, "fmi2Instantiate");
+  fmi2Component comp;
+  comp = fmi2Instantiate("hsInstance", type, "guid", "resourceLoc", &cbFuncs, false, false);
+  printf("C: Result of fmi2Instantiate %p", comp);
 
-  int a;
-  a=cfooF(5);
 
-  printf("\r\nResult of cfooF: %i\n", a);
-  
+
+  //  cbFuncs.allocateMemory = NULL;
+  //cbFuncs.freeMemory = NULL;
+  //cbFuncs.stepFinished = NULL;
+  //cbFuncs.componentEnvironment = NULL;
+
+  //foo fooF;
+  //fooF = dlsym(handle, "foo");
+  //int a;
+  //a=fooF(5);
+  //printf("C: Result of fooF: %i\r\n", a);
+
+  //instantiateLEG instantiateLEGF;
+  //bool b;
+  //char name[5] = {'H', 'F', 'M', 'U', '\0'};
+  //instantiateLEGF = dlsym(handle, "instantiateLEG");
+  //b = instantiateLEGF(name);
+  //printf("C: Result of instantiateLEGF: %i\r\n", b);
+
+  //instantiate instantiateF;
+  //void* c;
+  //instantiateF = dlsym(handle, "instantiate");
+  //c = instantiateF(name);
+  //printf("C: Result of instantiateF: %i\r\n", c);
+
+  //printEnv printEnvF;
+  //int d;
+  //printEnvF = dlsym(handle, "printEnv");
+  //d = printEnvF(c);
+  //printf("C: Result of printEnv: %i\r\n", d);
+
+  //updateEnv updateEnvF;
+  //bool e;
+  //updateEnvF = dlsym(handle, "updateEnv");
+  //d = updateEnvF(c);
+  //printf("C: Result of updateEnv: %i\r\n", e);
+
+  //int f;
+  //printEnvF = dlsym(handle, "printEnv");
+  //f = printEnvF(c);
+  //printf("C: Result of printEnv: %i\r\n", f);
+
   dlclose(handle);
 }
 
